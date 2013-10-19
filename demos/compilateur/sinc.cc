6cb38afa6a5182c65c8af32c80558576dffa06e7
Xavier Garrido <xavier.garrido@gmail.com>
2013-10-19 16:13:06 +0200

    real clean

--- demos/compilateur/sinc.cc
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-using namespace std;
-
-#define PI 3.14159265358979323846
-
-float sinc(float x)
-{
-  return sin(PI * x) / (PI * x);
-}
--- demos/compilateur/sinc.hh
+++
@@ -1,6 +0,0 @@
-#ifndef SINC_HH_INCLUDED
-#define SINC_HH_INCLUDED
-
-float sinc(float);
-
-#endif
